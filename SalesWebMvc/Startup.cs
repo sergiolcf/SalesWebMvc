1cbb2d8ad4ea1a7f8aeaf8942f2e3e5870b05719
Sergio Ferreira <sergiolc.ferreira@hotmail.com>
2020-10-15 21:06:58 -0300

    CODE-FIRST

--- SalesWebMvc/Startup.cs
+++ SalesWebMvc/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using SalesWebMvc.Data;
 
+
 namespace SalesWebMvc
 {
     public class Startup
@@ -36,8 +37,9 @@ namespace SalesWebMvc
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<SalesWebMvcContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("SalesWebMvcContext")));
+                    options.UseMySql(Configuration.GetConnectionString("SalesWebMvcContext"), builder => builder.MigrationsAssembly("SalesWebMvc")));
         }
+        
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
